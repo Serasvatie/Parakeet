0fa250aaec6c3dd83181b8d99db2a0df0660bda5
Serasvatie <quentin.favre@epitech.eu>
2016-10-26 13:45:33 +0200

    Basic C# class

---
+++ Parakeet/Parakeet/ViewModel/BaseNotifyPropertyChanged.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel;
+
+namespace Parakeet.ViewModel
+{
+    public class BaseNotifyPropertyChanged : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string name)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+
+        protected void RefreshAll()
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(""));
+        }
+    }
+}
---
+++ Parakeet/Parakeet/ViewModel/RelayCommand.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace Parakeet.ViewModel
+{
+    class RelayCommand : ICommand
+    {
+        private readonly Action _execute;
+        private readonly Func<bool> _canExecute;
+
+        event EventHandler ICommand.CanExecuteChanged
+        {
+            add
+            {
+                if (_canExecute != null)
+                    CommandManager.RequerySuggested += value;
+                else
+                    throw new NotImplementedException();
+            }
+            remove
+            {
+                if (_canExecute != null)
+                    CommandManager.RequerySuggested += value;
+                else
+                    throw new NotImplementedException();
+            }
+        }
+
+        public RelayCommand(Action execute)
+            : this(execute, null) { }
+
+        public RelayCommand(Action execute, Func<bool> canExecute)
+        {
+            if (execute == null)
+                throw new ArgumentException("Action : _execute");
+            this._execute = execute;
+            this._canExecute = canExecute;
+        }
+
+        bool ICommand.CanExecute(object parameter)
+        {
+            return _canExecute?.Invoke() ?? true;
+            throw new NotImplementedException();
+        }
+
+        void ICommand.Execute(object parameter)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
