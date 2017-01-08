a19257fd5dadece571bbd7b08ef50841eadfaf34
Quentin <quentin.favre@epitech.eu>
2017-01-08 20:20:59 +0100

    Begin Sort + Manager Class

--- Parakeet/Manager/SManager/SManager.cs
+++ Parakeet/Manager/SManager/SManager.cs
@@ -1,17 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
 using Manager.Manager;
 
 namespace SManager
 {
-    public class SManager
+    public class SManager : AManager
     {
-        private List<SortByRule> _sort = new List<SortByRule>();
-        private List<DirectoryModel> _directory = new List<DirectoryModel>();
-
-        public event EventHandler IsBwStarted;
-        public BackgroundWorker BwTask;
+        private string[] _input;
+        private string[] _output;
 
         public SManager()
         {
@@ -23,16 +23,54 @@ namespace SManager
             BwTask.DoWork += ExecuteTask;
         }
 
-        public void SettingList(Dictionary<string, object> lists)
+        public override void SettingList(Dictionary<string, object> lists)
         {
-            _directory = lists["Directories"] as List<DirectoryModel>;
+            DirectoryCover = lists["Directories"] as List<DirectoryModel>;
             dynamic tmp;
             if (lists.TryGetValue("SortingRules", out tmp))
-                _sort = tmp as List<SortByRule>;
+                SortRules = tmp as List<SortByRule>;
         }
 
-        private void ExecuteTask(object sender, DoWorkEventArgs e)
+        protected override void ExecuteTask(object sender, DoWorkEventArgs e)
         {
+            base.ExecuteTask(sender, e);
+
+            int res = 0;
+            List<Tuple<int, string>> output = new List<Tuple<int, string>>();
+
+            foreach (var des in DirectoryCover)
+            {
+                try
+                {
+                    if (!des.Activated)
+                        continue;
+                    DoSort(des.Path);
+                }
+                catch (Exception ex)
+                {
+                    output.Add(new Tuple<int, string>(-1, ex.Message));
+                }
+                if (BwTask.CancellationPending)
+                {
+                    e.Cancel = true;
+                    e.Result = res;
+                    return;
+                }
+                e.Result = res;
+            }
+
+        }
+
+        private void DoSort(string mainDirectory)
+        {
+            DirectoryInfo directoryInfo = new DirectoryInfo(mainDirectory);
+
+            var directories = from d in directoryInfo.GetDirectories("*", SearchOption.AllDirectories)
+                              where (d.Attributes & (FileAttributes.)
+            _input = Directory.GetDirectories(mainDirectory);
+            _output = Directory.GetDirectories(mainDirectory);
+
+
         }
     }
 }
