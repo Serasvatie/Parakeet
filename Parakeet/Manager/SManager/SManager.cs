e28d260e6da37216d18c6418aca4a7341a82606c
Serasvatie <quentin.favre@epitech.eu>
2017-01-30 15:41:40 +0100

    Sort to Test only regex

--- Parakeet/Manager/SManager/SManager.cs
+++ Parakeet/Manager/SManager/SManager.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.IO;
+using System.Text.RegularExpressions;
 using System.Linq;
 using System.Net.Sockets;
 using Manager.Manager;
@@ -44,7 +45,7 @@ namespace SManager
                 {
                     if (!des.Activated)
                         continue;
-                    DoSort(des.Path);
+                    res = DoSort(des.Path);
                 }
                 catch (Exception ex)
                 {
@@ -61,16 +62,50 @@ namespace SManager
 
         }
 
-        private void DoSort(string mainDirectory)
+        private int DoSort(string mainDirectory)
         {
-            DirectoryInfo directoryInfo = new DirectoryInfo(mainDirectory);
-
-            var directories = from d in directoryInfo.GetDirectories("*", SearchOption.AllDirectories)
-                              where (d.Attributes & (FileAttributes.)
+            int res = 0;
             _input = Directory.GetDirectories(mainDirectory);
-            _output = Directory.GetDirectories(mainDirectory);
+            for (int i = 0; i < _input.Length; i++)
+            {
+                var nDir = DoCheckRules(_input[i]);
+                if (nDir != "")
+                {
+                    moveDirectory(mainDirectory, _input[i], nDir);
+                    res++;
+                    int save = _input.Length;
+                    _input = Directory.GetDirectories(mainDirectory);
+                    if (save != _input.Length)
+                        i = 0;
+                }
+            }
+            return res;
+        }
 
+        private void moveDirectory(string mainDirectory, string target, string ndir)
+        {
+            if (Directory.Exists(mainDirectory + ndir))
+            {
+                Directory.Move(target, mainDirectory + "\\" + ndir + Path.GetFileName(target));
+            }
+            else
+            {
+                Directory.CreateDirectory(mainDirectory + "\\" + ndir);
+                Directory.Move(target, mainDirectory + "\\" + ndir + Path.GetFileName(target));
+            }
+        }
 
+        private string DoCheckRules(string p)
+        {
+            foreach (var rule in SortRules)
+            {
+                if (!rule.IsActivated)
+                    continue;
+                var tmp = Regex.Match(Path.GetFileNameWithoutExtension(p), rule.Strings).Groups[0].Value;
+                if (tmp != "")
+                    return tmp;
+            }
+            return "";
         }
     }
 }
