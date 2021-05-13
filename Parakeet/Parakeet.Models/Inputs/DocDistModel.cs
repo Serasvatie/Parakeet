011d0c5864354ac71a5e2d459788440815179796
Quentin Favre <quentin.favre@epitech.eu>
2021-05-13 13:16:45 +0200

    DocDist Case Sensitive
    Add option Sensitive case for the DocDist Task

--- Parakeet/Parakeet.Models/Inputs/DocDistModel.cs
+++ Parakeet/Parakeet.Models/Inputs/DocDistModel.cs
@@ -2,33 +2,41 @@
 
 namespace Parakeet.Models.Inputs
 {
-    public class DocDistModel
-    {
-        private int _threshold;
-        private Target _target;
+	public class DocDistModel
+	{
+		private int _threshold;
+		private Target _target;
+		private bool _caseSensitive;
 
-        public DocDistModel()
-        {
-            _threshold = 50;
-            _target = Target.Folder;
-        }
+		public DocDistModel()
+		{
+			_threshold = 50;
+			_target = Target.Folder;
+			_caseSensitive = true;
+		}
 
-        public DocDistModel(int _thres, Target target)
-        {
-            _threshold = _thres;
-            _target = target;
-        }
+		public DocDistModel(int _thres, Target target)
+		{
+			_threshold = _thres;
+			_target = target;
+		}
 
-        public int Threshold
-        {
-            get { return _threshold; }
-            set { _threshold = value; }
-        }
+		public int Threshold
+		{
+			get { return _threshold; }
+			set { _threshold = value; }
+		}
 
-        public Target Target
-        {
-            get { return _target; }
-            set { _target = value; }
-        }
-    }
+		public Target Target
+		{
+			get { return _target; }
+			set { _target = value; }
+		}
+
+		public bool CaseSensitive
+		{
+			get => _caseSensitive;
+			set => _caseSensitive = value;
+		}
+	}
 }
